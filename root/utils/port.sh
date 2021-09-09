150f789f063279df4a6dcd8fc81087fba295da60
Nico Franke <nico.franke01@gmail.com>
2021-09-09 19:26:35 +0200

    Update gluetun http url

--- root/utils/port.sh
+++ root/utils/port.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv bash
 
-PORT=$(curl -s localhost:8000/openvpn/portforwarded | jq -r '.port')
+PORT=$(curl -s localhost:8000/v1/openvpn/portforwarded | jq -r '.port')
 transmission-remote --auth $USER:$PASS --port $PORT > /dev/null
